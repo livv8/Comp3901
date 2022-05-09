19e089be826594eafc5dcf85325cf3c8f8232eeb
landocodes <blagrove87@gmail.com>
2022-05-09 15:01:31 -0500

    update login

--- csv/app.py
+++ csv/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 from os.path import join, dirname, realpath
-
-import pandas as pd 
+import csv
 import mysql.connector
 
 app = Flask(__name__)
 
-
+app.config['DEBUG'] = True
 #upload folder
 UPLOAD_FOLDER = 'static/files'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 #database
 mydb = mysql.connector.connect(
     host="localhost",
@@ -19,15 +17,7 @@ mydb = mysql.connector.connect(
     password="",
     database="inventory"
 )
-
-mycursor = mydb.cursor()
-
-mycursor.execute("SHOW DATABASES")
-
-#view All Database
-for x in mycursor:
-    print(x)
-
+print('database connected')
 
 #Root url
 @app.route("/")
@@ -43,21 +33,18 @@ def upload():
         #set the file path
         uploaded_file.save(file_path)
         #save the file
+        cursor =mydb.cursor()
+    csv_data = csv.reader(open(file_path))
+    for row in csv_data:
+        cursor.execute("""INSERT INTO ohs (Item_Number, Item_Description, Qty, Price, Department) VALUES (%s, %s, %s, %s, %s)""",row)
+    print(row)
+
+    mydb.commit()
+    cursor.close()
+    print("DONE")
+
     return redirect(url_for("index"))
 
-def parseCSV(filePath):
-    #cvs column Names
-    col_names = ['item_Number', 'item_Description', 'Qty', 'Price', 'Department']
-    #use pandas to parse the csv file
-    csvData = pd.read_csv(filePath,names=col_names, header=None)
-    #Loop through the Rows
-    for i,row in csvData.iterrows():
-        sql = "INSERT INTO store (item_Number, item_Description, Qty, Price, Department) VALUES (%s, %s, %s, %s, %s, %s)"
-        value = (row['item_Number'],row['item_Description'],row['Qty'],row['Price'],row['Department'])
-        mycursor.execute(sql, value, if_exists='append')
-        mydb.commit()
-        print(i,row['item_Number'],row['item_Description'],row['Qty'],row['Price'],row['Department'])
-
-
+    
 if __name__ == '__main__':
     app.run(port = 5000)
--- flaskproject/app.py
+++ flaskproject/app.py
@@ -6,13 +6,16 @@ import re
 
 app = Flask(__name__)
 
+app.config['DEBUG']= True
+
+app.secret_key = 'your secret key'
 
 
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'locatix'
+app.config['MYSQL_DB'] = 'pythonlogin'
 
 mysql = MySQL(app)
 
@@ -22,30 +25,30 @@ def base():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-     # Output message if something goes wrong...
     msg = ''
-     # Check if "username" and "password" POST requests exist (user submitted form)
+    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-                # Check if account exists using MySQL
+        # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
-                # If account exists in accounts table in out database
+        # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
             # Redirect to home page
-            return 'Logged in successfully!'
+            return redirect(url_for('home'))
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
-    return render_template("login.html", msg='')
+    # Show the login form with message (if any)
+    return render_template('login.html', msg=msg)
 
     # http://localhost:5000/python/logout - this will be the logout page
 @app.route('/pythonlogin/logout')
@@ -67,31 +70,31 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+
+        # Check if account exists using MySQL        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
     elif request.method == 'POST':
         # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
+    # Show registration form with message (if any)        
     return render_template("signup.html", msg=msg)
 
-    email = request.form['email']
-        # Check if account exists using MySQL        # Check if account exists using MySQL
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-    account = cursor.fetchone()
-    # If account exists show error and validation checks
-    if account:
-        msg = 'Account already exists!'
-    elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-        msg = 'Invalid email address!'
-    elif not re.match(r'[A-Za-z0-9]+', username):
-        msg = 'Username must contain only characters and numbers!'
-    elif not username or not password or not email:
-        msg = 'Please fill out the form!'
-    else:
-        # Account doesnt exists and the form data is valid, now insert new account into accounts table
-        cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-        mysql.connection.commit()
-        msg = 'You have successfully registered!'
            
 @app.route("/home")
 def home():
@@ -120,4 +123,4 @@ def products():
     return render_template("productsview.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=500)
